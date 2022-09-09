903b2d9115418544447c841a3d3042e390791607
LavanyaDaravath <daravathlavanya45@gmail.com>
2022-09-09 04:38:22 +0000

    Article Page solution

---
+++ Articles Page/README.md
@@ -0,0 +1,8 @@
+# ARTICLES PAGE
+
+## Observations
+
+- In this assignment, let's build a Articles Page.
+- by applying the concepts we learned till now.
+- You can use the Bootstrap concepts and CCBP UI Kit as well.
+- When clicked on each category card, the page should display the List of Topics.
---
+++ Articles Page/index.js
@@ -0,0 +1,95 @@
+@import url("https://fonts.googleapis.com/css2?family=Bree+Serif&family=Caveat:wght@400;700&family=Lobster&family=Monoton&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display+SC:ital,wght@0,400;0,700;1,700&family=Playfair+Display:ital,wght@0,400;0,700;1,700&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,700&display=swap");
+
+.articles-page-bg-container {
+    background-color: #225db0;
+    background-size: cover;
+    height: 100vh;
+}
+
+.heading {
+    color: #ffffff;
+    font-family: "Roboto";
+    font-size: 35px;
+    margin-top: 20px;
+    margin-left: 20px;
+    font-weight: bold;
+    padding: 5px;
+}
+
+.description {
+    color: #ffffff;
+    font-family: "Roboto";
+    font-size: 20px;
+    margin-top: 10px;
+    margin-left: 20px;
+    font-weight: bold;
+    padding: 5px;
+}
+
+.empower-course-card {
+    background-color: #ffffff;
+    width: 119px;
+    padding: 10px;
+    border-color: #cbd2d9;
+    border-radius: 10px;
+    border-width: 1px;
+    border-style: solid;
+    margin: 15px;
+    margin-top: 20px;
+    margin-left: 20px;
+    font-weight: bold;
+}
+
+.image {
+    width: 35px;
+    margin: 15px;
+
+}
+
+.title {
+    color: #1f2933;
+    font-family: "Roboto";
+    font-size: 16px;
+    padding: 2px;
+    margin-bottom: 25px;
+}
+
+.technology-bg-container {
+    background-color: #225db0;
+    background-size: cover;
+    padding: 25px;
+    margin: 25px;
+}
+
+.name {
+    color: #ffffff;
+    font-family: "Roboto";
+    font-size: 30px;
+    padding: 15px;
+}
+
+.list-details {
+    color: #ffffff;
+    font-family: "Roboto";
+    font-size: 18px;
+    margin: 15px;
+}
+
+.list-items {
+    padding: 15px;
+}
+
+.button {
+    text-align: center;
+}
+
+.button1 {
+    color: white;
+    background-color: #225db0;
+    height: 40px;
+    width: 70px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: white;
+    margin-top: 10px;
+}
